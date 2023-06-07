aa327ab16c110db392490e14e9807a76cbfc8875
ValeHenriquez <vale.belen.vhf@gmail.com>
2023-06-06 21:12:17 -0400

    refactor names

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-import Login from "@/components/Login";
+import LoginForm from "@/components/LoginForm";
 
 const LoginPage = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Login />
+            <LoginForm />
         </main>
     )
 }
