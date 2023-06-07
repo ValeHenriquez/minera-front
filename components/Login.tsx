a41b99d438b1d00146c53f4d00c1e421c4000d0a
ValeHenriquez <vale.belen.vhf@gmail.com>
2023-06-06 20:04:19 -0400

    add form

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,51 +1,65 @@
 import * as React from 'react';
+import { useForm } from "react-hook-form";
+
 import truck from './assets/img/vehiculo.png';
 
+type FormData = {
+    email: string,
+    password: string
+};
+
 const Login: React.FC = () => {
+
+    const { register, handleSubmit, reset } = useForm<FormData>();
+
+    //formState: { errors }, no estamos haciendo validaciones por ahora
+
+    const onSubmit = (data: FormData) => {
+        alert(JSON.stringify(data));
+
+        reset();
+    };
+
     return (
-        <div className={"bg-white px-10 py-20 rounded-3xl border-2 border-gray-100"}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <h1 className="text-black text-5xl font-semibold">Iniciar sesión</h1>
-                <div className="flex items-center justify-center animate-bounce" style={{ marginLeft: '10px' }}>
-                    <img src={truck.src} style={{ width: '50px' }} />
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={"bg-white px-10 py-20 rounded-3xl border-2 border-gray-100"}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h1 className="text-black text-5xl font-semibold">Iniciar sesión</h1>
+                    <div className="flex items-center justify-center animate-bounce" style={{ marginLeft: '10px' }}>
+                        <img src={truck.src} style={{ width: '50px' }} />
+                    </div>
                 </div>
-            </div>
-            <div className={"mt-8"}>
-                <div>
-                    <label className={"text-lg font-medium"}>Email</label>
-                    <form>
+                <div className={"mt-8"}>
+                    <div>
+                        <label className={"text-lg font-medium"}>Email</label>
                         <input
-                            name='email'
                             className='w-full rounded-3xl border-2 border-blue-400 rounder-xl p-4 mt-1 bg-ffff'
                             placeholder='Ingresa tu email'
                             type="email"
-
-
+                            {...register("email", { required: true })}
                         />
-                    </form>
-                </div>
-                <div className={"mt-8"}>
-                    <label className={"text-lg font-medium"}>Contraseña</label>
-                    <form>
+                    </div>
+                    <div className={"mt-8"}>
+                        <label className={"text-lg font-medium"}>Contraseña</label>
                         <input
-                            name='password'
                             className='w-full rounded-3xl border-2 border-blue-400 rounder-xl p-4 mt-1 bg-ffff'
                             placeholder='Ingresa tu contraseña'
                             type="password"
+                            {...register("password", { required: true })}
 
                         />
-                    </form>
-                </div>
-                <div className={"mt-8 flex flex-col gap-y-4"}>
-                    <button
-                        className={"activate:scale-[.98] activate:duration-75 hover:scale-[1.01] ease-in-out transition-all bg-blue-600 text-white text-lg font-bold rounded-xl py-3 border-blue-600"}>LOGIN
-                    </button>
-                </div>
-                <div className={"mt-8 flex items-center justify-center"}>
-                    <button className={"font-medium text-base text-blue-700"}>Acceso invitado</button>
+                    </div>
+                    <div className={"mt-8 flex flex-col gap-y-4"}>
+                        <button
+                            className={"activate:scale-[.98] activate:duration-75 hover:scale-[1.01] ease-in-out transition-all bg-blue-600 text-white text-lg font-bold rounded-xl py-3 border-blue-600"}>LOGIN
+                        </button>
+                    </div>
+                    <div className={"mt-8 flex items-center justify-center"}>
+                        <button className={"font-medium text-base text-blue-700"}>Acceso invitado</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </form>
     );
 }
 
