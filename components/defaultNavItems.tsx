94baba74b24cc9883e58d29d62e56b9b4ee319d0
ValeHenriquez <vale.belen.vhf@gmail.com>
2023-06-09 21:48:51 -0400

    allow modify pits

--- components/defaultNavItems.tsx
+++ components/defaultNavItems.tsx
@@ -1,36 +1,20 @@
 import React from "react";
-import {
-    CalendarIcon,
-    FolderIcon,
-    HomeIcon,
-    UserGroupIcon,
-} from "@heroicons/react/24/outline";
-// define a NavItem prop
+
+
+const pits = ["Esperanza", "Llano", "Tesoro"]; //Modificar la data con la BD
+
+// Define a NavItem type.
 export type NavItem = {
     label: string;
     href: string;
-    icon: React.ReactNode;
+    letter: string;
 };
 
-export const defaultNavItems: NavItem[] = [
-    {
-        label: "Dashboard",
-        href: "/",
-        icon: <HomeIcon className="w-6 h-6" />,
-    },
-    {
-        label: "Esperanza",
-        href: "/esperanza",
-        icon: <FolderIcon className="w-6 h-6" />,
-    },
-    {
-        label: "Llano",
-        href: "/llano",
-        icon: <FolderIcon className="w-6 h-6" />,
-    },
-    {
-        label: "Tesoro",
-        href: "/tesoro",
-        icon: <FolderIcon className="w-6 h-6" />,
-    },
-];
+export const defaultNavbarItems: NavItem[] = pits.map((pit: string) => {
+    return {
+        label: pit,
+        href: "#" + pit.toLowerCase(),
+        letter: pit[0].toUpperCase()
+    };
+});
+
