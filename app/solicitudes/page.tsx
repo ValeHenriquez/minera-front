fd410131d312d9cabfdf9ef77be7f39918536d75
ValeHenriquez <vale.belen.vhf@gmail.com>
2023-07-14 02:15:17 -0400

    add requests page

---
+++ app/solicitudes/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import RequestMenu from '@/components/RequestsMenu';
+
+
+export default function Home() {
+    return (
+        <main className="flex flex-grow justify-center min-h-screen items-center">
+            <RequestMenu />
+        </main>
+    )
+}
---
+++ components/RequestsMenu.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+
+export default function RequestsMenu() {
+    return (
+        <main className="flex flex-grow justify-center min-h-screen items-center">
+            <div className="h-1/2 flex justify-center p-1">
+                <div className="custom-box bg-sky-400 flex justify-start rounded-2xl p-3">
+                    <h1 className="text-xl text-black font-bold">SOLICITUDES DE ACCESO PENDIENTES</h1>
+                </div>
+            </div>
+        </main>
+    )
+}
