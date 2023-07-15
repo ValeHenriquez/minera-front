b87507759720b0011b7afb69542e4ee7d8521df3
SebaFuentes <sebafuentesp@gmail.com>
2023-07-14 21:07:22 -0300

    Comentarios explicativos

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@
 import Image from 'next/image'
 import TabsWithTables from "@/components/TabsWithTables";
 
-
+/**
+ * Esta función es la que se encarga de renderizar la página de reporte diario
+ * @returns El contenido de la página de reporte diario
+ */
 export default function Home() {
     return (
         <main className="flex flex-col flex-grow justify-center min-h-screen items-center">
-            DEFAULT PAGE - REPORTE DIARIO
+            DEFAULT PAGE - REPORTE DIARIO 
         </main>
     )
 }
